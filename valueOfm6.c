a800f16a056116705cafe9ca16c93f650d7388dc
Mondira Biswas <biswasmondira57@gmail.com>
2025-02-14 21:04:30 +0600

    Add files via upload

---
+++ valueOfm6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    printf("Input the value of m=");
+    scanf("%d",&m);
+    if(m!=0){
+    
+        if(m>0)
+        n=1;
+        else
+        n=-1;
+    }
+    else
+    n=0;
+
+    
+    printf("The value of m=%d\n",m);
+    printf("The value of n=%d\n",n);
+    return 0;
+}
